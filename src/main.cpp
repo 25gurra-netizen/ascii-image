60acec49f2987811039a1a168f4365f61366fea2
25gurra-netizen <25gurra@gmail.com>
2026-09-08 21:04:19 +0200

    test image

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+/*
+1. Get the filename from the command line.
+2. Check that the file exists/can be opened.
+3. Load the image.
+4. Retrieve its width and height.
+5. Print them to the terminal.
+6. Handle the case where the image can't be loaded.
+*/
