67103bff17fb8a485514f54d3d0cc08142c4a6db
25gurra-netizen <25gurra@gmail.com>
2026-09-09 14:37:13 +0200

    First version

--- src/main.cpp
+++ src/main.cpp
@@ -6,3 +6,28 @@
 5. Print them to the terminal.
 6. Handle the case where the image can't be loaded.
 */
+#include <iostream>
+#include <filesystem>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+int main(int argc, char* argv[]) {
+    // Step 1: Get the filename from the command line
+    if (argc != 2) {
+            std::cerr << "Usage: " << argv[0] << " <filename>\n";
+            return 1;
+    }
+    //2. Check that the file exists/can be opened.
+    if (!std::filesystem::exists(argv[1])){
+        std::cerr << "File does not exist:" << argv[1] << "\n";
+        return 1;
+    }
+    
+    int height, width, channel;
+    std::cout << "File exist:" << argv[1] << "\n";
+    unsigned char *image = stbi_load(argv[1], &width, &height, &channel, 0);
+    std::cout << "Height: " << height << "\n";
+    std::cout << "Width: " << width << "\n";
+    return 0;
+}
